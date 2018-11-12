2d7b6258adfa8c7f3b93224c55d899ac7584e6f1
Ricardo <ricardoslyox@gmail.com>
2018-11-12 21:12:11 +0000

    Added: Initial Client GUI Update: gitignore

---
+++ Client/src/Controllers/View/MessageController.java
@@ -0,0 +1,4 @@
+package Controllers.View;
+
+public class MessageController {
+}
--- Client/src/Main.java
+++ Client/src/Main.java
@@ -1,6 +1,22 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+public class Main  extends Application {
+
+    public static void main(String[] args) { launch(args); }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Views/Layouts/main.fxml"));
+        primaryStage.setTitle("Mega PD");
+
+        Scene scene = new Scene(root, 800, 600);
+
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
     }
 }
