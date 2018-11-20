49988197372960a50428729de620753c1773ca13
FilipeA <filipera1995@hotmail.com>
2018-11-20 02:04:00 +0000

    Added: PasswordHasher Helper

---
+++ Server/src/Helpers/PasswordHasher.java
@@ -0,0 +1,67 @@
+package Helpers;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Arrays;
+import java.util.Base64;
+
+/**
+ * Original code at: http://www.appsdeveloperblog.com/encrypt-user-password-example-java/
+ * <br>
+ *   <b>Example on how to use</b>
+ *   <pre>{@code
+ *         String providedPassword = "password";
+ *         //Store this
+ *         String securePassword = PasswordHasher.generateSecurePassword(providedPassword);
+ *         //Compare with this
+ *         boolean passwordMatch = PasswordHasher.verifyUserPassword(providedPassword, securePassword);
+ *  }</pre>
+ */
+public final class PasswordHasher {
+
+        private static final int ITERATIONS = 10000;
+        private static final int KEY_LENGTH = 256;
+        private static final String SALT = "EqdmPh53c9x33EygXpTpcoJvc4VXLK";
+        //to anyone looking at this, reminder this is a school project, so yeah its just to prove we can do it, we know this should be more secure
+
+        private static String getSalt() {
+            return SALT;
+        }
+
+        public static byte[] hash(char[] password) {
+            PBEKeySpec spec = new PBEKeySpec(password, SALT.getBytes(), ITERATIONS, KEY_LENGTH);
+            Arrays.fill(password, Character.MIN_VALUE);
+            try {
+                SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+                return skf.generateSecret(spec).getEncoded();
+            } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+                throw new AssertionError("Error while hashing a password: " + e.getMessage(), e);
+            } finally {
+                spec.clearPassword();
+            }
+        }
+        public static String generateSecurePassword(String password) {
+            String returnValue = null;
+            byte[] securePassword = hash(password.toCharArray());
+
+            returnValue = Base64.getEncoder().encodeToString(securePassword);
+
+            return returnValue;
+        }
+
+        public static boolean verifyUserPassword(String providedPassword, String securedPassword)
+        {
+            boolean returnValue ;
+
+            // Generate New secure password with the same salt
+            String newSecurePassword = generateSecurePassword(providedPassword);
+
+            // Check if two passwords are equal
+            returnValue = newSecurePassword.equalsIgnoreCase(securedPassword);
+
+            return returnValue;
+        }
+
+}
