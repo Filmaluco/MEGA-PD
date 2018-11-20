1c61c15e0b965e7e5193ee6cc0a2e0a5bb587032
FilipeA <filipera1995@hotmail.com>
2018-11-20 02:07:49 +0000

    Update PasswordHasher.java
    added javaDoc

--- Server/src/Helpers/PasswordHasher.java
+++ Server/src/Helpers/PasswordHasher.java
@@ -9,6 +9,9 @@ import java.util.Base64;
 
 /**
  * Original code at: http://www.appsdeveloperblog.com/encrypt-user-password-example-java/
+ * 
+ * @version 1.0
+ *
  * <br>
  *   <b>Example on how to use</b>
  *   <pre>{@code
@@ -26,42 +29,63 @@ public final class PasswordHasher {
         private static final String SALT = "EqdmPh53c9x33EygXpTpcoJvc4VXLK";
         //to anyone looking at this, reminder this is a school project, so yeah its just to prove we can do it, we know this should be more secure
 
-        private static String getSalt() {
+    /**
+     *
+     * @return PasswordHasher salt
+     */
+    private static String getSalt() {
             return SALT;
         }
 
-        public static byte[] hash(char[] password) {
-            PBEKeySpec spec = new PBEKeySpec(password, SALT.getBytes(), ITERATIONS, KEY_LENGTH);
-            Arrays.fill(password, Character.MIN_VALUE);
-            try {
-                SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-                return skf.generateSecret(spec).getEncoded();
-            } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
-                throw new AssertionError("Error while hashing a password: " + e.getMessage(), e);
-            } finally {
-                spec.clearPassword();
-            }
+    /**
+     * Hash's a given password in char char[]
+     * @param password password
+     * @return hashed version of the password
+     */
+    private static byte[] hash(char[] password) {
+        PBEKeySpec spec = new PBEKeySpec(password, SALT.getBytes(), ITERATIONS, KEY_LENGTH);
+        Arrays.fill(password, Character.MIN_VALUE);
+        try {
+            SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+            return skf.generateSecret(spec).getEncoded();
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            throw new AssertionError("Error while hashing a password: " + e.getMessage(), e);
+        } finally {
+            spec.clearPassword();
         }
-        public static String generateSecurePassword(String password) {
-            String returnValue = null;
-            byte[] securePassword = hash(password.toCharArray());
+    }
 
-            returnValue = Base64.getEncoder().encodeToString(securePassword);
+    /**
+     * Hash's a given password
+     * @param password plain text password
+     * @return hashed version of the password
+     */
+    public static String generateSecurePassword(String password) {
+        String returnValue = null;
+        byte[] securePassword = hash(password.toCharArray());
 
-            return returnValue;
-        }
+        returnValue = Base64.getEncoder().encodeToString(securePassword);
 
-        public static boolean verifyUserPassword(String providedPassword, String securedPassword)
-        {
-            boolean returnValue ;
+        return returnValue;
+    }
 
-            // Generate New secure password with the same salt
-            String newSecurePassword = generateSecurePassword(providedPassword);
+    /**
+     * Compares hashed password with plain text password
+     * @param providedPassword plain textPassword
+     * @param securedPassword hashed password
+     * @return true if they match
+     */
+    public static boolean verifyUserPassword(String providedPassword, String securedPassword)
+    {
+        boolean returnValue ;
 
-            // Check if two passwords are equal
-            returnValue = newSecurePassword.equalsIgnoreCase(securedPassword);
+        // Generate New secure password with the same salt
+        String newSecurePassword = generateSecurePassword(providedPassword);
 
-            return returnValue;
-        }
+        // Check if two passwords are equal
+        returnValue = newSecurePassword.equalsIgnoreCase(securedPassword);
+
+        return returnValue;
+    }
 
 }
