deb7c038efd431e3e5459ff267fb41577bd67a57
Vasooooo <vascoide650@hotmail.com>
2018-12-18 20:31:47 +0000

    Update
    Added Connection module
    Revamped FileManager
    Removed outdated class FileReceiverManager

---
+++ Client/src/Modules/Connection.java
@@ -0,0 +1,57 @@
+package Modules;
+
+import Core.Modules.EntityData;
+import Core.Modules.MegaPDRemoteModule;
+import Core.Modules.ModuleInterface;
+import Core.UserInfo;
+import MegaPD.Core.Exeptions.MegaPDRemoteException;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Map;
+
+    public class Connection extends MegaPDRemoteModule implements ModuleInterface.ConnectionModule {
+
+
+        public Connection(EntityData remoteData) {
+            super(remoteData);
+        }
+
+        @Override
+        public int login() throws MegaPDRemoteException, IOException {
+            return (Integer) this.remoteMethod(ConnectionRequest.guestLogin);
+        }
+
+        @Override
+        public int login(String s, String s1) throws MegaPDRemoteException, IOException {
+            return (Integer) this.remoteMethod(ConnectionRequest.AuthLogin, s, s1);
+        }
+
+        @Override
+        public Socket registerNotificationPort(int i) throws MegaPDRemoteException, IOException {
+            ServerSocket serverS = new ServerSocket(i);
+            this.remoteMethod(ConnectionRequest.registerNotification, serverS.getLocalPort());
+            return serverS.accept();
+        }
+
+        @Override
+        public void registerFileTransferPort(int i) throws MegaPDRemoteException, IOException {
+            this.remoteMethod(ConnectionRequest.registerFileTransfer, i);
+        }
+
+        @Override
+        public void logout() throws MegaPDRemoteException, IOException {
+            this.remoteMethod(ConnectionRequest.logout);
+        }
+
+        @Override
+        public Map<Integer, String> getUsersOnline() throws MegaPDRemoteException, IOException {
+            return (Map<Integer, String>) this.remoteMethod(ConnectionRequest.getUsersOnline);
+        }
+
+        @Override
+        public UserInfo getUser(int i) throws MegaPDRemoteException, IOException {
+            return (UserInfo) this.remoteMethod(ConnectionRequest.getUser, i);
+        }
+    }
