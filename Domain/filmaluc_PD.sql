5089f1f1f86fd1715ad8a281b88222287fc50c04
FilipeA <filipera1995@hotmail.com>
2018-12-24 03:36:07 +0000

    Update filmaluc_PD.sql

--- Domain/filmaluc_PD.sql
+++ Domain/filmaluc_PD.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: 19-Dez-2018 às 03:11
+-- Generation Time: 24-Dez-2018 às 03:35
 -- Versão do servidor: 5.6.37
 -- PHP Version: 7.1.8
 
@@ -32,7 +32,7 @@ CREATE TABLE IF NOT EXISTS `Files` (
   `Name` varchar(32) NOT NULL COMMENT 'File Name',
   `Directory` varchar(32) NOT NULL COMMENT 'File Directory',
   `Size` int(10) unsigned NOT NULL COMMENT 'File Size (MB)'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Files Information';
+) ENGINE=InnoDB AUTO_INCREMENT=60 DEFAULT CHARSET=latin1 COMMENT='Files Information';
 
 -- --------------------------------------------------------
 
@@ -44,23 +44,10 @@ CREATE TABLE IF NOT EXISTS `History` (
   `ID` int(10) unsigned NOT NULL COMMENT 'HistoryID',
   `OwnerID` int(10) unsigned NOT NULL COMMENT 'FK UserID [Owner]',
   `TargetID` int(10) unsigned NOT NULL COMMENT 'FK UserID [Target]',
-  `FileName` int(11) NOT NULL COMMENT 'File name at the moment of the transfer',
+  `FileName` varchar(32) NOT NULL COMMENT 'File name at the moment of the transfer',
   `Date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'File sent on ...',
   `Received` tinyint(4) NOT NULL DEFAULT '0' COMMENT '[SUCESS] = 1'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Transfer of files History';
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `Message`
---
-
-CREATE TABLE IF NOT EXISTS `Message` (
-  `ID` int(10) unsigned NOT NULL COMMENT 'Message ID',
-  `UserID` int(10) unsigned NOT NULL,
-  `Message` text NOT NULL COMMENT 'Message Text',
-  `CreationDate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Message Creation Date'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Users Messages';
 
 -- --------------------------------------------------------
 
@@ -74,7 +61,7 @@ CREATE TABLE IF NOT EXISTS `Servers` (
   `IP` varchar(32) NOT NULL COMMENT 'Server IP',
   `Port` int(11) NOT NULL COMMENT 'TCP Port',
   `Status` tinyint(4) NOT NULL DEFAULT '1' COMMENT '[ON] = 1'
-) ENGINE=InnoDB AUTO_INCREMENT=222 DEFAULT CHARSET=latin1 COMMENT='Server Info';
+) ENGINE=InnoDB AUTO_INCREMENT=72 DEFAULT CHARSET=latin1 COMMENT='Server Info';
 
 -- --------------------------------------------------------
 
@@ -107,28 +94,16 @@ CREATE TABLE IF NOT EXISTS `User` (
   `Username` varchar(32) DEFAULT NULL COMMENT 'User Username',
   `Password` varchar(255) DEFAULT NULL COMMENT 'User Passowrd',
   `Blocked` tinyint(4) NOT NULL DEFAULT '0' COMMENT '[BLOCKED] = 1'
-) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=113 DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `User`
 --
 
 INSERT INTO `User` (`ID`, `IP_Address`, `ConnectionTCP_Port`, `NotificationTCP_Port`, `FileTransferTCP_Port`, `Ping_UDP_Port`, `Name`, `Username`, `Password`, `Blocked`) VALUES
-(23, 'notDefinedYet', 0, 53405, 1, NULL, 'FilipeA', 'user1', 'd1e6wrgP1s1ZKYjMP3PhUjCWsqFb6OKbmVQiwKnWZbs=', 0),
-(24, 'notDefinedYet', 0, NULL, NULL, NULL, 'VascoA', 'user2', 'd1e6wrgP1s1ZKYjMP3PhUjCWsqFb6OKbmVQiwKnWZbs=', 0),
-(25, 'notDefinedYet', 0, NULL, NULL, NULL, 'RicardoS', 'user3', 'd1e6wrgP1s1ZKYjMP3PhUjCWsqFb6OKbmVQiwKnWZbs=', 0);
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `User_Messages`
---
-
-CREATE TABLE IF NOT EXISTS `User_Messages` (
-  `MessageID` int(10) unsigned NOT NULL COMMENT 'FK MessageID',
-  `TargetID` int(10) unsigned NOT NULL COMMENT 'Message to UserID [Reveiver]',
-  `Seen` tinyint(4) NOT NULL DEFAULT '0' COMMENT 'Seen'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+(23, '192.168.192.1', 0, 62316, 62314, NULL, 'FilipeA', 'user1', 'd1e6wrgP1s1ZKYjMP3PhUjCWsqFb6OKbmVQiwKnWZbs=', 0),
+(24, '192.168.192.1', 0, 62331, 62329, NULL, 'VascoA', 'user2', 'd1e6wrgP1s1ZKYjMP3PhUjCWsqFb6OKbmVQiwKnWZbs=', 0),
+(25, '192.168.1.110', 0, 58946, 58944, NULL, 'RicardoS', 'user3', 'd1e6wrgP1s1ZKYjMP3PhUjCWsqFb6OKbmVQiwKnWZbs=', 0);
 
 --
 -- Indexes for dumped tables
@@ -148,13 +123,6 @@ ALTER TABLE `History`
   ADD PRIMARY KEY (`ID`),
   ADD KEY `ServerID` (`OwnerID`,`TargetID`),
   ADD KEY `fk_history_receiver` (`TargetID`);
-
---
--- Indexes for table `Message`
---
-ALTER TABLE `Message`
-  ADD PRIMARY KEY (`ID`),
-  ADD KEY `UserID` (`UserID`);
 
 --
 -- Indexes for table `Servers`
@@ -177,13 +145,6 @@ ALTER TABLE `User`
   ADD UNIQUE KEY `Username` (`Username`);
 
 --
--- Indexes for table `User_Messages`
---
-ALTER TABLE `User_Messages`
-  ADD KEY `UserID` (`MessageID`,`TargetID`),
-  ADD KEY `fk_message_target` (`TargetID`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -191,27 +152,22 @@ ALTER TABLE `User_Messages`
 -- AUTO_INCREMENT for table `Files`
 --
 ALTER TABLE `Files`
-  MODIFY `ID` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'File ID';
+  MODIFY `ID` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'File ID',AUTO_INCREMENT=60;
 --
 -- AUTO_INCREMENT for table `History`
 --
 ALTER TABLE `History`
   MODIFY `ID` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'HistoryID';
 --
--- AUTO_INCREMENT for table `Message`
---
-ALTER TABLE `Message`
-  MODIFY `ID` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Message ID';
---
 -- AUTO_INCREMENT for table `Servers`
 --
 ALTER TABLE `Servers`
-  MODIFY `ID` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=222;
+  MODIFY `ID` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=72;
 --
 -- AUTO_INCREMENT for table `User`
 --
 ALTER TABLE `User`
-  MODIFY `ID` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'User ID',AUTO_INCREMENT=26;
+  MODIFY `ID` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'User ID',AUTO_INCREMENT=113;
 --
 -- Constraints for dumped tables
 --
@@ -230,24 +186,11 @@ ALTER TABLE `History`
   ADD CONSTRAINT `fk_history_receiver` FOREIGN KEY (`TargetID`) REFERENCES `User` (`ID`);
 
 --
--- Limitadores para a tabela `Message`
---
-ALTER TABLE `Message`
-  ADD CONSTRAINT `fk_message_user` FOREIGN KEY (`UserID`) REFERENCES `User` (`ID`);
-
---
 -- Limitadores para a tabela `Server_Users`
 --
 ALTER TABLE `Server_Users`
   ADD CONSTRAINT `conn_server` FOREIGN KEY (`ServerID`) REFERENCES `Servers` (`ID`),
   ADD CONSTRAINT `fk_server_user` FOREIGN KEY (`UserID`) REFERENCES `User` (`ID`);
-
---
--- Limitadores para a tabela `User_Messages`
---
-ALTER TABLE `User_Messages`
-  ADD CONSTRAINT `conn_message` FOREIGN KEY (`MessageID`) REFERENCES `Message` (`ID`),
-  ADD CONSTRAINT `fk_message_target` FOREIGN KEY (`TargetID`) REFERENCES `User` (`ID`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
